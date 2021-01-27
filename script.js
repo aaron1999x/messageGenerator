42ec44fd74b1cce1ff195a254c861264d0d390c1
aaron1999x <aarontys1999@gmail.com>
2021-01-27 15:27:46 -0600

    finished script

--- script.js
+++ script.js
@@ -3,17 +3,36 @@
 function generateMessage(){
 
 
+    let randomNumber = Math.floor(Math.random()* 100);
+    
     const recommendation ={
 
         dressCode: ['Casual','Business Casual','Smart Casual','Semi Casual','Formal'],
         activity: ['read a book','play a computer game','watch a movie','bake some cookies','go out for a jog',
                     'listen to some music','play golf','have friends over for a sleepover',
                     'go to the gym','learn a new skill','clean up your bedroom','call your parents',
-                    'do some homework',]
+                    'do some homework',],
+    };
+
+    
+
+    function getRandom(arr){
+
+        let getLength = arr.length;
+        let randomIndex = Math.round(Math.random()*getLength);
+        return arr[randomIndex];
+    };
+
+    
+    return `Your lucky number today is ${randomNumber}. Why not try out dressing ${getRandom(recommendation.dressCode)} today. Today you should ${getRandom(recommendation.activity)}`;
+    
+
+    //console.log(recommendation.dressCode[0]);
 
 
 
-
-    }
+    
 
 }
+
+console.log(generateMessage());
