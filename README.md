e9849d8c09bbf9aac16cda46e1cc8e4cda0a0295
aaron1999x <aarontys1999@gmail.com>
2021-01-27 13:15:11 -0600

    updated readme and started a roughdraft of the script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # messageGenerator
+### Every time a user runs a program, they should get a new, randomized output. The message that it outputs should be made up of at least three different pieces of data.
+## **Idea**
+1. output lucky number 
+2. recommend dress code
+3. recommend and activity 
---
+++ script.js
@@ -0,0 +1,19 @@
+//Every time a user runs a program, they should get a new, randomized output.
+
+function generateMessage(){
+
+
+    const recommendation ={
+
+        dressCode: ['Casual','Business Casual','Smart Casual','Semi Casual','Formal'],
+        activity: ['read a book','play a computer game','watch a movie','bake some cookies','go out for a jog',
+                    'listen to some music','play golf','have friends over for a sleepover',
+                    'go to the gym','learn a new skill','clean up your bedroom','call your parents',
+                    'do some homework',]
+
+
+
+
+    }
+
+}
